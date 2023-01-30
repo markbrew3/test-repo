8304863fc1558d7bb67c1f0b3b2224d1788da7f5
Mark Brewer <markbrewer369@gmail.com>
2023-01-30 00:28:50 -0800

    data setup

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log('In here!');
+let log = console.log;
+import { refData } from "C:\Users\markb\workspace\foundations\test-repo\referenceData.js"";
+
+function renderToDom (divId, html) {
+  const selectedDiv = document.querySelector(divId);// render elements to the dom dynamicly
+  selectedDiv.innerHTML = html;
+}
+
+const renderCards = ()
